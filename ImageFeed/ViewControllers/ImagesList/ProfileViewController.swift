7ab6cfe29489f731bfde4e254ec53c973c18b630
Konstantin Kirillov <kirilloffkostya@gmail.com>
2023-01-08 15:02:23 +0700

    add profile screen

---
+++ ImageFeed/ViewControllers/ImagesList/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Konstantin Kirillov on 08.01.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userLoginLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func exitButtonTapped() {
+        
+    }
+    
+}
