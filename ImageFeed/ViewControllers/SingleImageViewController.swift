29bd270ab5c4adb50704243ca8a6fe2be4ea33d0
Konstantin Kirillov <kirilloffkostya@gmail.com>
2023-01-11 13:46:42 +0700

    add sale method and zoom option for imageSingleScreen

--- ImageFeed/ViewControllers/SingleImageViewController.swift
+++ ImageFeed/ViewControllers/SingleImageViewController.swift
@@ -9,22 +9,50 @@ import UIKit
 
 class SingleImageViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var scrollView: UIScrollView!
     
     var image: UIImage! {
         didSet {
             guard isViewLoaded else { return }
             imageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        rescaleAndCenterImageInScrollView(image: image)
+        
+        scrollView.delegate = self
+        scrollView.maximumZoomScale = 1.25
+        scrollView.minimumZoomScale = 0.1
     }
     
     @IBAction func backwardButtonTapped() {
         dismiss(animated: true)
     }
-    
+}
 
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
 }
