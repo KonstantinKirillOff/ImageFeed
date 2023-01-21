aa507e77ebf726d3d0ec0db82dcce30780db2f9a
Konstantin Kirillov <kirilloffkostya@gmail.com>
2023-01-21 23:35:29 +0300

    add constants for Unsplash Avtorization

---
+++ ImageFeed/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Konstantin Kirillov on 21.01.2023.
+//
+
+import Foundation
+
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
+let AccessKey = "DMGBd_bhpgkA43sh0XwoPis264uPwYAHhxnDGdQVaaI"
+let SecretKey = "gLMPi52tnl_lGIl8WLh70nW6Bq1XhqZzbmdZmgfPFyI"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
