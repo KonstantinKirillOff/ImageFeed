46d7c9b7bba6c59b3e4ed221b567c3c6c6efb807
Konstantin Kirillov <kirilloffkostya@gmail.com>
2023-01-24 22:21:53 +0300

    add AuthVC

---
+++ ImageFeed/ViewControllers/AuthVC/WebViewViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Konstantin Kirillov on 24.01.2023.
+//
+
+import UIKit
+
+final class WebViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
